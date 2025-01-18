ec46503e358ac606869fe4a60aabe469d69b3ba2
Debashis Debnath <frankendeba@Debashiss-MacBook-Air.local>
2025-01-18 18:57:53 +0530

    invalid command

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,15 @@ fn main() {
     io::stdout().flush().unwrap();
 
     // Wait for user input
-    let stdin = io::stdin();
+    let stdin: io::Stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    println!("{}: command not found", input.trim());
+
+    // match input {
+    //     _ => println!("{}: command not found", input.trim()),
+    //     _err => println!("errrr!!!!"),
+    // };
+
+    let out = io::stdout().write_all(b"Hello world");
 }
